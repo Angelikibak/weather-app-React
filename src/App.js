265170ba02d914db82ae17a049116c6a0cf3973d
Angeliki Bakogianni <agelikibakogianni@gmail.com>
2021-06-25 18:08:21 +0300

    Fetching data from API - cors solution with proxy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
+import React,{useState, useEffect} from 'react'
 
-import './App.css';
+const App = () => {
+  async function FetchWeatherData  () {
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    const value= 'München'
+    const APIkey = 'b6907d289e10d714a6e88b30761fae22';
+    const forecast = `data/2.5/forecast?q=${value}&APPID=${APIkey}`;
+
+    fetch(forecast)
+      .then((res) => {
+        if (res.ok ) {
+          return res.json();
+        }
+        throw Error(res.statusText);
+      })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  };
+
+  useEffect(() => {
+  FetchWeatherData()
+  }, [])
+   
+    
+            
+  
+    return (
+      <div></div>
+    )
 }
 
-export default App;
+
+export default App
