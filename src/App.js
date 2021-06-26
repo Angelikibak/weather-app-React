e2a626ac1d40fdbbbdc79a94e8599f73dbd06654
Angeliki Bakogianni <agelikibakogianni@gmail.com>
2021-06-26 16:25:09 +0300

    Formatting the app with prettier

--- src/App.js
+++ src/App.js
@@ -1,173 +1,183 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 import Weather from './components/weather/weather';
-import Loading from './components/loading/loading'
+import Loading from './components/loading/loading';
 
 const kelvin = 273;
 
-
 const App = () => {
-  const [weatherInfo, setWeatherInfo] = useState(null)
+      const [weatherInfo, setWeatherInfo] = useState(null);
 
-  const weatherTime = [0,1,2,3,4,5,6,7]
+      const weatherTime = [0, 1, 2, 3, 4, 5, 6, 7];
 
-  async function FetchWeatherData  () {
+      async function FetchWeatherData() {
+            const value = 'München';
+            const APIkey = 'b6907d289e10d714a6e88b30761fae22';
+            const forecast = `data/2.5/forecast?q=${value}&APPID=${APIkey}`;
 
+            fetch(forecast)
+                  .then((res) => {
+                        if (res.ok) {
+                              return res.json();
+                        }
+                        throw Error(res.statusText);
+                  })
+                  .then((data) => {
+                        const months = [
+                              'January',
+                              'February',
+                              'March',
+                              'April',
+                              'May',
+                              'June',
+                              'July',
+                              'August',
+                              'September',
+                              'October',
+                              'Nocvember',
+                              'December',
+                        ];
 
-    const value= 'München'
-    const APIkey = 'b6907d289e10d714a6e88b30761fae22';
-    const forecast = `data/2.5/forecast?q=${value}&APPID=${APIkey}`;
+                        const days = [
+                              'Sunday',
+                              'Monday',
+                              'Tuesday',
+                              'Wednesday',
+                              'Thursday',
+                              'Friday',
+                              'Saturday',
+                        ];
+                        const currentDate = new Date();
+                        const date = `${
+                              days[currentDate.getDay()]
+                        } ${currentDate.getDate()} ${
+                              months[currentDate.getMonth()]
+                        }`;
 
+                        const temp = data.list[0].main.temp - kelvin;
 
-    fetch(forecast)
-      .then((res) => {
-        if (res.ok ) {
-          return res.json();
-        }
-        throw Error(res.statusText);
-      })
-    .then((data) => {
-      const months = [  
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'Nocvember',
-        'December',
-      ];
+                        const weatherInfo = {
+                              city: 'Munich',
+                              date,
+                              description: data.list[0].weather[0].description,
+                              temp,
+                              highestTemp: data.list[0].main.temp_max,
+                              lowestTemp: data.list[0].main.temp_min,
+                              forecast: data.list,
+                        };
 
+                        setWeatherInfo(weatherInfo);
+                  })
+                  .catch((error) => {
+                        console.log(error);
+                  });
+      }
 
-      const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-      const currentDate = new Date();
-      const date = `${days[currentDate.getDay()]} ${currentDate.getDate()} ${
-        months[currentDate.getMonth()]
-      }`;
+      useEffect(() => {
+            FetchWeatherData();
+      }, []);
 
-
-      
-      const temp = data.list[0].main.temp - kelvin;
-
-
-      const weatherInfo = {
-        city: 'Munich',
-        date,
-        description: data.list[0].weather[0].description,
-        temp,
-        highestTemp: data.list[0].main.temp_max,
-        lowestTemp: data.list[0].main.temp_min,
-        forecast: data.list,
-      };
-
-
-      setWeatherInfo(weatherInfo)
-    })
-    .catch(error => {
-      console.log(error);
-    });
-  };
-
-
-  useEffect(() => {
-  FetchWeatherData()
-  }, [])
-   
-  let city = "Munich";
-      let date = "";
-      let temp=""; 
-      let highestTemp="";
-      let description="";
-      let MainIcon= "04d";
-      let Url=""
-      let lowestTemp=""
-      if(weatherInfo){
-          city = weatherInfo.city
-          date = weatherInfo.date
-          temp = (weatherInfo.temp ).toFixed(0)
-          highestTemp= (weatherInfo.highestTemp).toFixed(0)
-          description= weatherInfo.description
-          lowestTemp = (weatherInfo.lowestTemp).toFixed(0)
-          MainIcon = weatherInfo.forecast[0].weather[0].icon
-          Url= ""
-          switch (MainIcon){
-           
-              case "01d":
-                Url = './icons/sunny.svg'
-                break;
-                case "02d":
-                Url = './icons/mostly-sunny.svg'
-                  break;
-                  case "03d":
-                 Url = './icons/mostly-sunny.svg'
-                    break;
-                    case "04d":
-                  Url = './icons/mostly-sunny.svg'
-                      break;
-                      case "09d":
-                   Url = './icons/mostly-sunny.svg'
+      let city = 'Munich';
+      let date = '';
+      let temp = '';
+      let highestTemp = '';
+      let description = '';
+      let MainIcon = '04d';
+      let Url = '';
+      let lowestTemp = '';
+      if (weatherInfo) {
+            city = weatherInfo.city;
+            date = weatherInfo.date;
+            temp = weatherInfo.temp.toFixed(0);
+            highestTemp = weatherInfo.highestTemp.toFixed(0);
+            description = weatherInfo.description;
+            lowestTemp = weatherInfo.lowestTemp.toFixed(0);
+            MainIcon = weatherInfo.forecast[0].weather[0].icon;
+            Url = '';
+            switch (MainIcon) {
+                  case '01d':
+                        Url = './icons/sunny.svg';
                         break;
-                  case "10d":
-                   Url = './icons/mostly-sunny.svg'
-                          break;
-                  case "11d":
-                    Url = './icons/mostly-sunny.svg'
-                            break;
-                  case "13d":
-                    Url = './icons/mostly-sunny.svg'
-                            break;
-                   case "50d":
-                         Url = './icons/mostly-sunny.svg'
-                                        break;
+                  case '02d':
+                        Url = './icons/mostly-sunny.svg';
+                        break;
+                  case '03d':
+                        Url = './icons/mostly-sunny.svg';
+                        break;
+                  case '04d':
+                        Url = './icons/mostly-sunny.svg';
+                        break;
+                  case '09d':
+                        Url = './icons/mostly-sunny.svg';
+                        break;
+                  case '10d':
+                        Url = './icons/mostly-sunny.svg';
+                        break;
+                  case '11d':
+                        Url = './icons/mostly-sunny.svg';
+                        break;
+                  case '13d':
+                        Url = './icons/mostly-sunny.svg';
+                        break;
+                  case '50d':
+                        Url = './icons/mostly-sunny.svg';
+                        break;
 
+                  default:
+                        Url = './icons/sunny.svg';
+            }
+      }
 
-            default:
-    
-              Url = './icons/sunny.svg';
-          }
-      }  
-            
-  
-    return (
-      weatherInfo ? 
-      <main className ="main-container">
-      <div className="current-temperature">
-          <div className="current-temperature__icon-container">
-            <img src={Url} className="current-temperature__icon" alt="" /> 
-          </div>
-          <div className="current-temperature__content-container">
-            <div className="current-temperature__city">
-              <span className="current-temperature__summary">{description}</span>
-              <span className="current-stats__value">{highestTemp - kelvin} &deg;/{lowestTemp - kelvin} &deg; </span>
-                <span className="location-and-date__location">{city}</span>
-              <div>
-            </div>
-          </div>
-          <div className = "current-temperature__date">
-            <div className="current-temperature__value">{temp}&deg;</div>
-            <div className="location-and-date__date">{date}</div>
-          </div>
-          </div>
-        </div>
+      return weatherInfo ? (
+            <main className='main-container'>
+                  <div className='current-temperature'>
+                        <div className='current-temperature__icon-container'>
+                              <img
+                                    src={Url}
+                                    className='current-temperature__icon'
+                                    alt=''
+                              />
+                        </div>
+                        <div className='current-temperature__content-container'>
+                              <div className='current-temperature__city'>
+                                    <span className='current-temperature__summary'>
+                                          {description}
+                                    </span>
+                                    <span className='current-stats__value'>
+                                          {highestTemp - kelvin} &deg;/
+                                          {lowestTemp - kelvin} &deg;{' '}
+                                    </span>
+                                    <span className='location-and-date__location'>
+                                          {city}
+                                    </span>
+                                    <div></div>
+                              </div>
+                              <div className='current-temperature__date'>
+                                    <div className='current-temperature__value'>
+                                          {temp}&deg;
+                                    </div>
+                                    <div className='location-and-date__date'>
+                                          {date}
+                                    </div>
+                              </div>
+                        </div>
+                  </div>
 
+                  <div className='weather-by-hour'>
+                        <div className='weather-by-hour__container'>
+                              {weatherInfo &&
+                                    weatherTime.map((item, index) => (
+                                          <Weather
+                                                weatherInfo={weatherInfo}
+                                                id={item}
+                                                key={item}
+                                          />
+                                    ))}
+                        </div>
+                  </div>
+            </main>
+      ) : (
+            <Loading />
+      );
+};
 
-        <div className="weather-by-hour">
-          <div className="weather-by-hour__container">
-        {weatherInfo && weatherTime.map((item, index)=>(
-                      <Weather weatherInfo={weatherInfo} id={item} key={item}/>
-              )
-            
-              )} 
-              </div>
-              </div>
-        </main> : <Loading />
-    )
-}
-
-
-
-
-export default App
+export default App;
--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -1,59 +1,55 @@
-import React from 'react'
+import React from 'react';
 import moment from 'moment';
-import './weather.scss'
+import './weather.scss';
 
-const Weather = ({weatherInfo, id}) => {
-    const kelvin = 273;
-    let {dt} =weatherInfo.forecast[id]
-    let {temp } = weatherInfo.forecast[id].main
-    temp = (temp - kelvin).toFixed(0)
-    let icon = weatherInfo.forecast[id].weather[0].icon
-    let Url= ""
-    switch (icon){
-     
-        case "01d":
-          Url = './icons/sunny.svg'
-          break;
-          case "02d":
-          Url = './icons/mostly-sunny.svg'
-            break;
-            case "03d":
-           Url = './icons/mostly-sunny.svg'
-              break;
-              case "04d":
-            Url = './icons/mostly-sunny.svg'
-                break;
-                case "09d":
-             Url = './icons/mostly-sunny.svg'
-                  break;
-            case "10d":
-             Url = './icons/mostly-sunny.svg'
-                    break;
-            case "11d":
-              Url = './icons/mostly-sunny.svg'
-                      break;
-            case "13d":
-              Url = './icons/mostly-sunny.svg'
-                      break;
-             case "50d":
-                   Url = './icons/sunny.svg'
-                                  break;
+const Weather = ({ weatherInfo, id }) => {
+   const kelvin = 273;
+   let { dt } = weatherInfo.forecast[id];
+   let { temp } = weatherInfo.forecast[id].main;
+   temp = (temp - kelvin).toFixed(0);
+   let icon = weatherInfo.forecast[id].weather[0].icon;
+   let Url = '';
+   switch (icon) {
+      case '01d':
+         Url = './icons/sunny.svg';
+         break;
+      case '02d':
+         Url = './icons/mostly-sunny.svg';
+         break;
+      case '03d':
+         Url = './icons/mostly-sunny.svg';
+         break;
+      case '04d':
+         Url = './icons/mostly-sunny.svg';
+         break;
+      case '09d':
+         Url = './icons/mostly-sunny.svg';
+         break;
+      case '10d':
+         Url = './icons/mostly-sunny.svg';
+         break;
+      case '11d':
+         Url = './icons/mostly-sunny.svg';
+         break;
+      case '13d':
+         Url = './icons/mostly-sunny.svg';
+         break;
+      case '50d':
+         Url = './icons/sunny.svg';
+         break;
 
       default:
+         Url = './icons/sunny.svg';
+   }
+   return (
+      <div className='weather-by-hour__item'>
+         <div className='weather-by-hour__hour'>
+            {moment(dt * 1000).format('HH:mm ')}
+         </div>
+         <img className='weather-by-hour__logo' src={Url} alt='Mostly sunny' />
+         <div className='weather-by-hour__degree'>{temp}&deg;</div>
+      </div>
+   );
+};
 
-        Url = './icons/sunny.svg';
-    }
-    return (
-
-      
-        
-    <div className="weather-by-hour__item">
-      <div className="weather-by-hour__hour">{moment(dt* 1000).format('HH:mm ')}</div>
-      <img className="weather-by-hour__logo" src={Url} alt="Mostly sunny" />
-      <div className="weather-by-hour__degree">{temp}&deg;</div>
-    </div>   
-        
-    )
-}
-
-export default Weather
+export default Weather;
