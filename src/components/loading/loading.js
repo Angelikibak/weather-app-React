ac2e00a6b11338513f773873630d8548158f5015
Angeliki Bakogianni <agelikibakogianni@gmail.com>
2021-06-26 17:06:16 +0300

    Formatting loading.js

--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Loading = () => (
-      <div>
-            <h1>
-                  The app is Loading
-                  <span>.</span>
-                  <span>.</span>
-                  <span>.</span>
-            </h1>
-      </div>
+   <div>
+      <h1>
+         The app is Loading
+         <span>.</span>
+         <span>.</span>
+         <span>.</span>
+      </h1>
+   </div>
 );
 
 export default Loading;
